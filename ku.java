9c9b035e1c317b71d238290437c794d1cf4775b4
Krishna K <krish1826s@gmail.com>
2024-12-17 09:32:38 +0530

    Delete ku.java

--- ku.java
+++
@@ -1,7 +0,0 @@
-
-private static void leftShiftUnitDigits(int arr[], int N) {
-    int fd=arr[0];
-    for(int i=0;i<N-1;i++){
-        arr[i]=(arr[i]/10)*10+arr[i+1]%10;
-    }arr[N-1]=(arr[N-1]/10)*10+fd%10;
-}
